30d1b437c7ace339e3f6b8a79527e00aeb746ca8
EvgenijRonenko <evgenij.ronenko@gmail.com>
2022-12-20 17:16:06 +0100

    yarn install

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
 import React from 'react';
 import './App.css';
 
+
 function App() {
-  return (
-    <div className="App">
-     Hello, samurai! Let's go!
-    </div>
-  );
+    return (
+        <div className="App">
+            App Hello
+        </div>
+    );
+}
+
+type MessageType = {
+    message: string
+}
+
+const HelloMessage = (props: MessageType) => {
+    return <h1>{props.message}</h1>
 }
 
 export default App;
