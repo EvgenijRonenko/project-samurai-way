b3cf47905507500588d9e8c1e8c0f3f0e0cfce89
EvgenijRonenko <evgenij.ronenko@gmail.com>
2022-12-20 21:40:20 +0100

    yarn install

--- src/App.tsx
+++ src/App.tsx
@@ -2,20 +2,55 @@ import React from 'react';
 import './App.css';
 
 
-function App() {
+const App = () => {
     return (
-        <div className="App">
-            App Hello
+        <div className="app-wrapper">
+            <header className="header">
+                <img src="https://seeklogo.com/images/I/IMG-logo-931DB0D0DA-seeklogo.com.gif"/>
+            </header>
+            <nav className="nav">
+                <div>
+                    <a>Profile</a>
+                </div>
+                <
+                    div>
+                    <a>Messages</a>
+                </div>
+                <
+                    div>
+                    <a>News</a>
+                </div>
+                <div>
+                    <a>Music</a>
+                </div>
+                <div>
+                    <a>Settings</a>
+                </div>
+            </nav>
+            <div className="content">
+                <div>
+                    <img src="https://mirpozitiva.ru/wp-content/uploads/2019/11/1472042791_25.jpg"/>
+                </div>
+                <div>
+                    ava + description
+                </div>
+                <div>
+                    My posts
+                    <div>
+                        New post
+                    </div>
+                    <div>
+                        <div>
+                            post 1
+                        </div>
+                        <div>
+                            post 2
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
-    );
-}
-
-type MessageType = {
-    message: string
-}
-
-const HelloMessage = (props: MessageType) => {
-    return <h1>{props.message}</h1>
+    )
 }
 
 export default App;
